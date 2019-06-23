5ea1b2df88a8757f584236a68e95f01dc6e88723
sergioitazar <itazar@gmail.com>
2019-06-23 11:48:11 -0300

    Exercicio 1 da aula 4

---
+++ Aula4exe1/src/br/com/digitalhouse/PrincipalAula4.java
@@ -0,0 +1,27 @@
+package br.com.digitalhouse;
+
+public class PrincipalAula4 {
+    public static void main(String[] args) {
+
+
+        //Exercicio 1
+        Integer umNumeroA;
+        Double umNumeroB;
+        String umaCadeiaDeTexto;
+        umNumeroA = 30;
+        umNumeroB = 20.0;
+        umaCadeiaDeTexto = "Uma Cadeia de Texto";
+
+        double soma = umNumeroA + umNumeroB;
+
+        System.out.println("Soma dos numeros ");
+        System.out.println(soma);
+
+        double diferenca = umNumeroA - umNumeroB;
+
+
+        System.out.println("Diferença dos números ");
+        System.out.println(diferenca);
+
+    }
+}
